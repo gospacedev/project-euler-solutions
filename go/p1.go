40e1777cd2ead84240bc1da2fa2e518c3e01c6ea
Grantley Cullar <grantcullar@gmail.com>
2023-04-23 21:32:26 +0800

    create first go solution

--- go/p1.go
+++ go/p1.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func sum(arr []int) int {
+    sum := 0
+    for _, value := range arr {
+        sum += value
+    }
+    return sum
+}
+
 func main() {
-	
+	var a []int
+
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			a = append(a, i)
+		}
+	}
+
+	ans := sum(a)
+
+	fmt.Println(ans)
 }
