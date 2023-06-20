f48da04861ebb19cbc9af4cc20ecb3477a9771f6
Grantley Cullar <grantcullar@gmail.com>
2023-06-20 18:22:38 +0800

    add java solution

--- java/p1.java
+++ java/p1.java
@@ -1,5 +1,5 @@
 public class p1 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int n = 0;
         for (int i = 0; i < 1000; i++) {
             if (i % 3 == 0  || i % 5 == 0) {
