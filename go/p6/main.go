852abfe5adf2a251638988edf4abf5644d8382cc
Grantley Cullar <grantcullar@gmail.com>
2023-06-21 22:09:23 +0800

    add p6 go solution

---
+++ go/p6/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	sumOfSqaures := 0
+	squareOfSums := 0
+
+	for i := 0; i <= 100; i++ {
+		sumOfSqaures += int(math.Pow(float64(i), 2))
+
+		squareOfSums += i
+	}
+
+	squareOfSums = int(math.Pow(float64(squareOfSums), 2))
+
+	n := squareOfSums - sumOfSqaures
+
+	fmt.Println(n)
+	fmt.Println(sumOfSqaures)
+	fmt.Println(squareOfSums)
+}
