b4dcd45307c2bfbb097b4d2d00963a2c834cbaf6
Grantley Cullar <grantcullar@gmail.com>
2023-06-27 00:00:22 +0800

    add p9.cpp

---
+++ cpp/p9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+auto findPythagoreanTriplet(int sum)
+{
+    for (size_t a = 1; a < sum / 3; a++)
+    {
+        for (size_t b = 0; b < sum / 2; b++)
+        {
+            int c = sum - a - b;
+            if (a * a + b * b == c * c)
+            {
+                return a * b * c;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int ans = findPythagoreanTriplet(1000);
+    std::cout << ans << std::endl;
+    return 0;
+}
