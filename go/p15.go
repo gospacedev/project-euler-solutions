0a91b3f26c5d582b971c1dd73a41de34426b1eb6
Grantley Cullar <grantcullar@gmail.com>
2023-08-18 17:37:41 +0800

    update p16 go solution

--- go/p15.go
+++ go/p15.go
@@ -14,7 +14,7 @@ func p15() float64 {
 		n float64  = k * 2
 	)
 
-	// Solve for the amount all the of possible routes from (0, 0) to (20, 20) using combination formula
+	// Solve for the amount all the of possible routes from (0, 0) to (20, 20) using the combination formula
 	ans := factorial(n) / (factorial(n-k) * factorial(k))
 	return ans
 }
